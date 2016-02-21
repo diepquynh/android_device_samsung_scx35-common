8315976e69f1c69aea6e231b92229a410f7fd7c5
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-21 02:57:44 +0200

    scx35-common: Allow vendor init to override props
    Change-Id: Ibf8aa134c7fae061ff3c71c05a505eb83fccc80d
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- init/init_sec.cpp
+++ init/init_sec.cpp
@@ -25,6 +25,8 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -65,6 +67,17 @@ device_variant match(std::string bl)
 device_variant find_device_variant() {
 	bootloader = GetProperty("ro.bootloader", "");
 	return match(bootloader);
+}
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
 void vendor_load_properties()
