a633306907a9bd534bf1c466fda104d2f1e0425c
Remilia Scarlet <remilia.1505@gmail.com>
2017-01-22 20:43:50 +0700

    RIL: Remove broken responseIccCardStatus()
    It's already like that from the beginning
    
    Change-Id: I9cf800c9dd37d1984c663f2e4e282ea1ecdc80b5
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- ril/SC7730SERIL.java
+++ ril/SC7730SERIL.java
@@ -51,47 +51,6 @@ public class SC7730SERIL extends SamsungSPRDRIL implements CommandsInterface {
         super(context, preferredNetworkType, cdmaSubscription, instanceId);
     }
 
-    @Override
-    protected Object
-    responseIccCardStatus(Parcel p) {
-        IccCardApplicationStatus appStatus;
-
-        IccCardStatus cardStatus = new IccCardStatus();
-        cardStatus.setCardState(p.readInt());
-        cardStatus.setUniversalPinState(p.readInt());
-        cardStatus.mGsmUmtsSubscriptionAppIndex = p.readInt();
-        cardStatus.mCdmaSubscriptionAppIndex = p.readInt();
-        cardStatus.mImsSubscriptionAppIndex = p.readInt();
-
-        int numApplications = p.readInt();
-
-        // limit to maximum allowed applications
-        if (numApplications > IccCardStatus.CARD_MAX_APPS) {
-            numApplications = IccCardStatus.CARD_MAX_APPS;
-        }
-        cardStatus.mApplications = new IccCardApplicationStatus[numApplications];
-
-        for (int i = 0 ; i < numApplications ; i++) {
-            appStatus = new IccCardApplicationStatus();
-            appStatus.app_type       = appStatus.AppTypeFromRILInt(p.readInt());
-            appStatus.app_state      = appStatus.AppStateFromRILInt(p.readInt());
-            appStatus.perso_substate = appStatus.PersoSubstateFromRILInt(p.readInt());
-            appStatus.aid            = p.readString();
-            appStatus.app_label      = p.readString();
-            appStatus.pin1_replaced  = p.readInt();
-            appStatus.pin1           = appStatus.PinStateFromRILInt(p.readInt());
-            appStatus.pin2           = appStatus.PinStateFromRILInt(p.readInt());
-            p.readInt(); // pin1_num_retries
-            p.readInt(); // puk1_num_retries
-            p.readInt(); // pin2_num_retries
-            p.readInt(); // puk2_num_retries
-            p.readInt(); // perso_unblock_retries
-
-            cardStatus.mApplications[i] = appStatus;
-        }
-        return cardStatus;
-    }
-
     // This thing... it causes lots of headaches due to RIL crashes
     @Override
     public void
