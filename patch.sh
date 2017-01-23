ad404384746980d0c9de8efa9d1f6cd4a7888b33
Remilia Scarlet <remilia.1505@gmail.com>
2017-01-23 12:05:31 +0700

    patches: Remove frameworks_base patch
    From this commit: d3afe8ca0b066ff8cf7d483652d81c3eb551ca26
    We have fixed multitasking button, so we don't need this patch anymore
    
    Change-Id: I9bea7ff542cbf4ad82e10416d25b39c22cfcc199
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- patch.sh
+++ patch.sh
@@ -5,11 +5,9 @@ LOCALDIR=$(pwd)
 cp ${LOCALDIR}/patches/build.patch ../../../build
 cp ${LOCALDIR}/patches/external_tinyalsa.patch ../../../external/tinyalsa
 cp ${LOCALDIR}/patches/frameworks_av.patch ../../../frameworks/av
-cp ${LOCALDIR}/patches/frameworks_base.patch ../../../frameworks/base
 
 # Apply them
 cd ../../..
 cd build && patch -p1 < *.patch
 cd ../external/tinyalsa && patch -p1 < *.patch
 cd ../../frameworks/av && patch -p1 < *.patch
-cd ../../frameworks/base && patch -p1 < *.patch
