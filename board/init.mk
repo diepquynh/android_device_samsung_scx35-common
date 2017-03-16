a2e373ba8b4469baafa4cd30dd31747bbca5e36d
Martin Bouchet <tincho5588@gmail.com>
2017-03-16 18:14:14 -0300

    scx35-common: Shim libGLES_mali.so
    Our mali blob is missing the __popcountsi2 symbol.
    Lets build it and shim the blob!
    
    Needs the following commit
    https://review.lineageos.org/#/c/165951
    
    Change-Id: I625ec9b8bc5a59cd330479914993ed8271984689
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- board/init.mk
+++ board/init.mk
@@ -1,3 +1,6 @@
 # Init
 TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_sec
+
+# SHIM GLES mali libraries
+LINKER_FORCED_SHIM_LIBS := /system/lib/egl/libGLES_mali.so|libpopcountsi2.so
