e23cbf4ab413ff0f38dc403d76267d88d974a21a
Remilia Scarlet <remilia.1505@gmail.com>
2017-04-07 17:33:04 +0700

    BoardConfig: Move WiFi flags to device
    Older bcm4343 cards can use CM's binaries, while newer can't
    
    Change-Id: I8be5013ab885325288443582a826b76174feac89
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,22 +37,6 @@ BOARD_CUSTOM_BT_CONFIG := device/samsung/scx35-common/bluetooth/libbt_vndcfg.txt
 # FM radio
 BOARD_HAVE_FM_BCM := true
 
-# Wifi
-BOARD_WLAN_DEVICE := bcmdhd
-BOARD_WLAN_DEVICE_REV := bcm4343
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA := "/system/etc/wifi/bcmdhd_sta.bin"
-WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/bcmdhd_apsta.bin"
-WIFI_DRIVER_NVRAM_PATH_PARAM := "/sys/module/dhd/parameters/nvram_path"
-WIFI_DRIVER_NVRAM_PATH := "/system/etc/wifi/nvram_net.txt"
-WIFI_BAND := 802_11_ABG
-BOARD_HAVE_SAMSUNG_WIFI := true
-
 # Graphics
 BOARD_EGL_NEEDS_HANDLE_VALUE := true
 TARGET_REQUIRES_SYNCHRONOUS_SETSURFACE := true
