6ef7f334bc812b6dc2e184d77841f3f041b8e4fa
karthick111 <karthickrko61@gmail.com>
2017-08-20 06:08:58 +0000

    scx35-common: Remove deprecated flags
    Change-Id: I14fcc30e5e855c7cc69ae3e86a6966b892110357
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- board/dexopt.mk
+++ board/dexopt.mk
@@ -1,7 +1,3 @@
-# Use dmalloc() for such low memory devices like us
-MALLOC_SVELTE := true
-BOARD_USES_LEGACY_MMAP := true
-
 # Enable dex-preoptimization to speed up the first boot sequence
 #WITH_DEXPREOPT := true
 #WITH_DEXPREOPT_PIC := true
--- board/platform.mk
+++ board/platform.mk
@@ -5,7 +5,5 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a7
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_NO_BOOTLOADER := true
 BOARD_VENDOR := samsung
