1d3fe0717b59350409d1a520768b1bd14931a187
Remilia Scarlet <remilia.1505@gmail.com>
2017-10-22 03:51:56 +0000

    scx35-common: Build power HAL
    Change-Id: I2f06029efcc023e9f249bd5e12815dd8db06aa00
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- product/hidl.mk
+++ product/hidl.mk
@@ -19,6 +19,7 @@ PRODUCT_PACKAGES += \
 	android.hardware.sensors@1.0-impl \
 	android.hardware.usb@1.0-service \
 	android.hardware.vibrator@1.0-impl \
+	android.hardware.power@1.0-impl \
 	android.hardware.wifi@1.0-service
 
 PRODUCT_COPY_FILES += \
