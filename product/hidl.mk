606766b8849a635029ac8cae59fc51f17883a600
Remilia Scarlet <remilia.1505@gmail.com>
2017-10-22 03:53:59 +0000

    scx35-common: Rearrange HIDL list
    Just for a better look
    
    Change-Id: Ib7d370816cdaf29bedbdc10764ea57c8aa024e15
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- product/hidl.mk
+++ product/hidl.mk
@@ -1,26 +1,26 @@
 # HIDL (HAL Interface Definition Language)
 PRODUCT_PACKAGES += \
-	android.hardware.bluetooth@1.0-impl \
-	android.hardware.camera.provider@2.4-impl.legacy \
-	camera.device@1.0-impl.legacy \
-	android.hardware.drm@1.0-impl \
-	android.hardware.graphics.allocator@2.0-impl \
-	android.hardware.graphics.allocator@2.0-service \
-	android.hardware.graphics.composer@2.1-impl \
-	android.hardware.graphics.mapper@2.0-impl \
-	android.hardware.gnss@1.0-impl \
-	android.hardware.light@2.0-impl \
-	android.system.net.netd@1.0 \
-	android.hardware.power@1.0-impl \
 	android.hardware.audio@2.0-impl \
 	android.hardware.audio.effect@2.0-impl \
-	android.hardware.broadcastradio@1.0-impl \
-	android.hardware.radio@1.0-impl \
-	android.hardware.sensors@1.0-impl \
-	android.hardware.usb@1.0-service \
-	android.hardware.vibrator@1.0-impl \
-	android.hardware.power@1.0-impl \
-	android.hardware.wifi@1.0-service
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.camera.provider@2.4-impl.legacy \
+    android.hardware.drm@1.0-impl \
+    android.hardware.gnss@1.0-impl \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.light@2.0-impl \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-impl \
+    android.hardware.radio@1.0-impl \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.usb@1.0-service \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.wifi@1.0-service \
+    android.system.net.netd@1.0 \
+    camera.device@1.0-impl.legacy
 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/manifest.xml:system/vendor/manifest.xml
