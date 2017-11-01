065d0e568f0117131186c9b4709ba400b22d8dde
Remilia Scarlet <remilia.1505@gmail.com>
2017-11-01 15:42:50 +0000

    scx35-common: Copy-pasta seccomp_policy instead
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- board/codecs.mk
+++ board/codecs.mk
@@ -1,5 +1,2 @@
 # Codecs
 BOARD_CANT_REALLOCATE_OMX_BUFFERS := true
-
-# seccomp
-BOARD_SECCOMP_POLICY := device/samsung/scx35-common/seccomp
--- product/codecs.mk
+++ product/codecs.mk
@@ -9,3 +9,7 @@ PRODUCT_PACKAGES += \
 	libstagefright_sprd_vpxdec \
 	libstagefright_sprd_aacdec \
 	libstagefright_sprd_mp3dec
+
+# Seccomp
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
