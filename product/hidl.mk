ac4539633cbd02df7cb7b829da3bc28c72ce3e6a
Remilia Scarlet <remilia.1505@gmail.com>
2017-11-17 14:48:48 +0000

    scx35-common: Again, remove HIDL graphics HAL
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- product/hidl.mk
+++ product/hidl.mk
@@ -8,8 +8,6 @@ PRODUCT_PACKAGES += \
 	android.hardware.configstore@1.0-impl \
 	android.hardware.drm@1.0-impl \
 	android.hardware.gnss@1.0-impl \
-	android.hardware.graphics.composer@2.1-impl \
-	android.hardware.graphics.mapper@2.0-impl \
 	android.hardware.keymaster@3.0-impl \
 	android.hardware.light@2.0-impl \
 	android.hardware.memtrack@1.0-impl \
