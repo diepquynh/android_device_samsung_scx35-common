71bb7f72c99a014f982ec6f9703ab561977a727c
Remilia Scarlet <remilia.1505@gmail.com>
2018-02-03 18:14:20 +0000

    scx35-common: Import bluedroid config
    This contains commit:
    commit 26a40307204b723887b9c5ccbfe74040d09613ac
    Author: Prateek Chaubey <chaubeyprateek@gmail.com>
    Date:   Mon, 29 Jan 2018 15:44:29 +0000
    
        land: Unset BTM_DEF_LOCAL_NAME
    
        * Automatically set bluetooth name based on ro.product.model
    
        Change-Id: Ie5659765b27d923c2d27405d694656d1361c4853
    
    Change-Id: Ibcb015c469e20bb8504ca42e8bb938fc1ad4e3d0
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -27,6 +27,7 @@ BOARD_VENDOR := samsung
 
 # Bluetooth
 USE_BLUETOOTH_BCM4343 := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/scx35-common/bluetooth
 BOARD_CUSTOM_BT_CONFIG := device/samsung/scx35-common/bluetooth/libbt_vndcfg.txt
 
 # RIL
