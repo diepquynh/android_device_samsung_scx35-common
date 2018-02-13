3b7de32c75e77a2bb037761ae9e625e7aad9577d
Remilia Scarlet <remilia.1505@gmail.com>
2018-02-13 09:21:59 +0000

    scx35-common: Build HIDL HALs
    Change-Id: Id47e8e594c09eab4f5a7e1ef1a309398f9fe3eb9
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- common.mk
+++ common.mk
@@ -144,6 +144,30 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	dalvik.vm.dex2oat-flags=--no-watch-dog
 
+# HIDL (HAL Interface Definition Language)
+PRODUCT_PACKAGES += \
+	android.hardware.audio@2.0-impl \
+	android.hardware.audio.effect@2.0-impl \
+	android.hardware.bluetooth@1.0-impl \
+	android.hardware.broadcastradio@1.0-impl \
+	android.hardware.camera.provider@2.4-impl.legacy \
+	android.hardware.configstore@1.0-impl \
+	android.hardware.drm@1.0-impl \
+	android.hardware.gnss@1.0-impl \
+	android.hardware.keymaster@3.0-impl \
+	android.hardware.light@2.0-impl \
+	android.hardware.memtrack@1.0-impl \
+	android.hardware.power@1.0-impl \
+	android.hardware.radio@1.0 \
+	android.hardware.radio.deprecated@1.0 \
+	android.hardware.renderscript@1.0-impl \
+	android.hardware.sensors@1.0-impl \
+	android.hardware.usb@1.0-service \
+	android.hardware.vibrator@1.0-impl \
+	android.hardware.wifi@1.0-service \
+	android.system.net.netd@1.0 \
+ 	camera.device@1.0-impl.legacy
+
 # Permissions
 PERMISSIONS_XML_FILES := \
 	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml \
