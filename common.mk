684fe06b075d5b8faac9d62570c447753a8d7cf7
Remilia Scarlet <remilia.1505@gmail.com>
2018-02-21 19:19:13 +0000

    scx35-common: Build graphics HIDL HALs
    Change-Id: I9fa9525a33a299a72f0f4fe0c6608e11c80cc1f4
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- common.mk
+++ common.mk
@@ -165,6 +165,8 @@ PRODUCT_PACKAGES += \
 	android.hardware.configstore@1.0-impl \
 	android.hardware.drm@1.0-impl \
 	android.hardware.gnss@1.0-impl \
+	android.hardware.graphics.allocator@2.0-impl \
+	android.hardware.graphics.mapper@2.0-impl \
 	android.hardware.keymaster@3.0-impl \
 	android.hardware.light@2.0-impl \
 	android.hardware.memtrack@1.0-impl \
