6e2fc2f4f55141b19c942e6d49cdd3f45d77689f
Remilia Scarlet <remilia.1505@gmail.com>
2018-06-16 13:31:04 +0000

    scx35-common: Use binderized gralloc
    Change-Id: I84d0253d35c5b15c69617f93f598a1f86179aafd
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- common.mk
+++ common.mk
@@ -164,6 +164,7 @@ PRODUCT_PACKAGES += \
 	android.hardware.drm@1.0-impl \
 	android.hardware.gnss@1.0-impl \
 	android.hardware.graphics.allocator@2.0-impl \
+	android.hardware.graphics.allocator@2.0-service \
 	android.hardware.graphics.mapper@2.0-impl \
 	android.hardware.keymaster@3.0-impl \
 	android.hardware.light@2.0-impl \
