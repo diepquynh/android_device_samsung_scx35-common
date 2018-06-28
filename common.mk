984ad01499ec4c4b05775f79665caad4e3e8673d
Remilia Scarlet <remilia.1505@gmail.com>
2018-06-28 06:43:57 +0000

    scx35-common: Split HIDL list to its own makefile
    Change-Id: I8049f84d9bd3404b14fca4af34853ce47997bf4c
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- common.mk
+++ common.mk
@@ -167,28 +167,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	pm.dexopt.shared=quicken
 
 # HIDL (HAL Interface Definition Language)
-PRODUCT_PACKAGES += \
-	android.hardware.audio@2.0-impl \
-	android.hardware.audio.effect@2.0-impl \
-	android.hardware.bluetooth@1.0-impl \
-	android.hardware.broadcastradio@1.0-impl \
-	android.hardware.camera.provider@2.4-impl-legacy \
-	android.hardware.configstore@1.0-impl \
-	android.hardware.gnss@1.0-impl \
-	android.hardware.graphics.mapper@2.0-impl \
-	android.hardware.keymaster@3.0-impl \
-	android.hardware.light@2.0-impl \
-	android.hardware.memtrack@1.0-impl \
-	android.hardware.power@1.0-service.sc8830 \
-	android.hardware.radio@1.0 \
-	android.hardware.radio.deprecated@1.0 \
-	android.hardware.renderscript@1.0-impl \
-	android.hardware.sensors@1.0-impl \
-	android.hardware.usb@1.0-service \
-	android.hardware.vibrator@1.0-impl \
-	android.hardware.wifi@1.0-service \
-	android.system.net.netd@1.0 \
- 	camera.device@1.0-impl-legacy
+include $(LOCAL_PATH)/hidl.mk
 
 # Permissions
 PERMISSIONS_XML_FILES := \
---
+++ hidl.mk
@@ -0,0 +1,71 @@
+# Audio
+PRODUCT_PACKAGES += \
+	android.hardware.audio@2.0-impl \
+	android.hardware.audio.effect@2.0-impl
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+	android.hardware.bluetooth@1.0-impl
+
+# Boardcast service
+PRODUCT_PACKAGES += \
+	android.hardware.broadcastradio@1.0-impl
+
+# Camera
+PRODUCT_PACKAGES += \
+	android.hardware.camera.provider@2.4-impl-legacy \
+ 	camera.device@1.0-impl-legacy
+
+# ConfigStore
+PRODUCT_PACKAGES += \
+	android.hardware.configstore@1.0-impl
+
+# GPS
+PRODUCT_PACKAGES += \
+	android.hardware.gnss@1.0-impl
+
+# Graphics
+PRODUCT_PACKAGES += \
+	android.hardware.graphics.mapper@2.0-impl
+
+# Keymaster
+PRODUCT_PACKAGES += \
+	android.hardware.keymaster@3.0-impl
+
+# Lights
+PRODUCT_PACKAGES += \
+	android.hardware.light@2.0-impl
+
+# Memtrack
+PRODUCT_PACKAGES += \
+	android.hardware.memtrack@1.0-impl
+
+# PowerHAL
+PRODUCT_PACKAGES += \
+	android.hardware.power@1.0-service.sc8830
+
+# RIL
+PRODUCT_PACKAGES += \
+	android.hardware.radio@1.0 \
+	android.hardware.radio.deprecated@1.0
+
+# RenderScript
+PRODUCT_PACKAGES += \
+	android.hardware.renderscript@1.0-impl
+
+# Sensors
+PRODUCT_PACKAGES += \
+	android.hardware.sensors@1.0-impl
+
+# USB
+PRODUCT_PACKAGES += \
+	android.hardware.usb@1.0-service
+
+# Vibrator
+PRODUCT_PACKAGES += \
+	android.hardware.vibrator@1.0-impl \
+
+# WiFi
+PRODUCT_PACKAGES += \
+	android.hardware.wifi@1.0-service \
+	android.system.net.netd@1.0
